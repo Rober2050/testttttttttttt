781e7ae1fdf609d2290ad0349dc88a2c3ebdf165
Rober2050 <79279856+Rober2050@users.noreply.github.com>
2021-08-30 19:57:07 -0300

    Initial commit ( Mensaje)

---
+++ app/src/main/java/com/example/testttttttttttt/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.testttttttttttt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val tag = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btn= findViewById<Button>(R.id.btnTest)
+        btn.setOnClickListener{
+            val txt = findViewById<TextView>(R.id.txtSaludo)
+            txt.text="Se pulso el botón"
+            Log.d(tag, "Boton pulsado")
+        }
+
+        Log.d(tag, "Paso por OnCreate")
+
+
+    }
+
+        override fun onStart() {
+            Log.d(tag, "pasando por OnStart")
+            super.onStart()
+
+        }
+
+     override fun onResume(){
+         Log.d(tag,"Estoy pasando por OnResume")
+         super.onResume()
+     }
+
+      override fun onPause(){
+          Log.d(tag,"Estoy pasando por onPause")
+          super.onPause()
+      }
+
+    override fun onStop(){
+        Log.d(tag, "Etoy pasando por onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(tag,"Estoy pasando por onDestroy")
+        super.onDestroy()
+    }
+
+    }
+
+
+
+
